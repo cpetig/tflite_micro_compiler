0a9650af7dbcaf5ada6ae48229fb79c57a325e55
Rafael Stahl <r.stahl@tum.de>
2020-05-20 18:17:33 +0200

    apply google code style

--- examples/hello_world.cc
+++ examples/hello_world.cc
@@ -1,11 +1,12 @@
 
+#include <iostream>  // for check output
+
 #include "tensorflow/lite/micro/kernels/all_ops_resolver.h"
 #include "tensorflow/lite/micro/micro_error_reporter.h"
 #include "tensorflow/lite/micro/micro_interpreter.h"
 #include "tensorflow/lite/micro/simple_memory_allocator.h"
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
-#include <iostream> // for check output
 
 // Create an area of memory to use for input, output, and intermediate arrays.
 // The size of this will depend on the model you're using, and may need to be
@@ -14,58 +15,57 @@ static const int tensor_arena_size = 6 * 1024;
 static uint8_t tensor_arena[tensor_arena_size];
 
 extern const unsigned char g_model[];
-//extern const int g_model_len;
+// extern const int g_model_len;
 
 // Set up logging.
-static tflite::ErrorReporter *error_reporter = nullptr;
+static tflite::ErrorReporter* error_reporter = nullptr;
 // This pulls in all the operation implementations we need.
-static tflite::ops::micro::AllOpsResolver *resolver = nullptr;
+static tflite::ops::micro::AllOpsResolver* resolver = nullptr;
 static const tflite::Model* model = nullptr;
-static tflite::MicroInterpreter *interpreter = nullptr;
+static tflite::MicroInterpreter* interpreter = nullptr;
 
-void init(void)
-{
-	static tflite::MicroErrorReporter micro_error_reporter;
-	error_reporter = &micro_error_reporter;
+void init(void) {
+  static tflite::MicroErrorReporter micro_error_reporter;
+  error_reporter = &micro_error_reporter;
 
-	// Map the model into a usable data structure. This doesn't involve any
-	// copying or parsing, it's a very lightweight operation.
-	model = ::tflite::GetModel(g_model);
-	if (model->version() != TFLITE_SCHEMA_VERSION) {
-		TF_LITE_REPORT_ERROR(error_reporter,
-			"Model provided is schema version %d not equal "
-			"to supported version %d.\n",
-			model->version(), TFLITE_SCHEMA_VERSION);
-		return;
-	}
-	static tflite::ops::micro::AllOpsResolver local_resolver;
-	resolver= &local_resolver;
+  // Map the model into a usable data structure. This doesn't involve any
+  // copying or parsing, it's a very lightweight operation.
+  model = ::tflite::GetModel(g_model);
+  if (model->version() != TFLITE_SCHEMA_VERSION) {
+    TF_LITE_REPORT_ERROR(error_reporter,
+                         "Model provided is schema version %d not equal "
+                         "to supported version %d.\n",
+                         model->version(), TFLITE_SCHEMA_VERSION);
+    return;
+  }
+  static tflite::ops::micro::AllOpsResolver local_resolver;
+  resolver = &local_resolver;
 
-	// Build an interpreter to run the model with.
-	static tflite::MicroInterpreter static_interpreter(model, *resolver, tensor_arena, tensor_arena_size, error_reporter);
-	interpreter= &static_interpreter;
-	TfLiteStatus allocate_status = interpreter->AllocateTensors();
-	if (allocate_status != kTfLiteOk) {
-		TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
-		return;
-	}
+  // Build an interpreter to run the model with.
+  static tflite::MicroInterpreter static_interpreter(
+      model, *resolver, tensor_arena, tensor_arena_size, error_reporter);
+  interpreter = &static_interpreter;
+  TfLiteStatus allocate_status = interpreter->AllocateTensors();
+  if (allocate_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+    return;
+  }
 }
 
-void run()
-{
-	TfLiteTensor* model_input = interpreter->input(0);
-    model_input->data.f[0] = 1.57f; // roughly PI/2
+void run() {
+  TfLiteTensor* model_input = interpreter->input(0);
+  model_input->data.f[0] = 1.57f;  // roughly PI/2
 
-	TfLiteStatus invoke_status = interpreter->Invoke();
-	if (invoke_status != kTfLiteOk) {
-		TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed");
-	}
-	TfLiteTensor* model_output = interpreter->output(0);
-    std::cerr << "result " << model_output->data.f[0] << std::endl;
+  TfLiteStatus invoke_status = interpreter->Invoke();
+  if (invoke_status != kTfLiteOk) {
+    TF_LITE_REPORT_ERROR(error_reporter, "Invoke failed");
+  }
+  TfLiteTensor* model_output = interpreter->output(0);
+  std::cerr << "result " << model_output->data.f[0] << std::endl;
 }
 
 int main(int argc, char** argv) {
-	init();
-	run();
-	return 0;
+  init();
+  run();
+  return 0;
 }
--- fake_implementations.cpp
+++ fake_implementations.cpp
@@ -2,48 +2,82 @@
 #include "tensorflow/lite/c/builtin_op_data.h"
 #include "tensorflow/lite/micro/kernels/all_ops_resolver.h"
 
-namespace tflite { namespace ops { namespace micro {
-    namespace reduce_max { extern TfLiteStatus Eval(TfLiteContext*, TfLiteNode*) { return kTfLiteOk; } }
-    TfLiteRegistration *Register_REDUCE_MAX(void) {
-        static TfLiteRegistration res = {
-            nullptr, nullptr, nullptr, reduce_max::Eval,
-        };
-        return &res;
-    }
-    namespace exp { extern TfLiteStatus Eval(TfLiteContext*, TfLiteNode*) { return kTfLiteOk; } }
-    TfLiteRegistration *Register_EXP(void) {
-        static TfLiteRegistration res = {
-            nullptr, nullptr, nullptr, exp::Eval,
-        };
-        return &res;
-    }
-    namespace sum { extern TfLiteStatus Eval(TfLiteContext*, TfLiteNode*) { return kTfLiteOk; } }
-    TfLiteRegistration *Register_SUM(void) {
-        static TfLiteRegistration res = {
-            nullptr, nullptr, nullptr, sum::Eval,
-        };
-        return &res;
-    }
-    namespace div { extern TfLiteStatus Eval(TfLiteContext*, TfLiteNode*) { return kTfLiteOk; } }
-    TfLiteRegistration *Register_DIV(void) {
-        static TfLiteRegistration res = {
-            nullptr, nullptr, nullptr, div::Eval,
-        };
-        return &res;
-    }
-    namespace squeeze { extern TfLiteStatus Eval(TfLiteContext*, TfLiteNode*) { return kTfLiteOk; } }
-    TfLiteRegistration *Register_SQUEEZE(void) {
-        static TfLiteRegistration res = {
-            nullptr, nullptr, nullptr, squeeze::Eval,
-        };
-        return &res;
-    }
-}}}
+namespace tflite {
+namespace ops {
+namespace micro {
+namespace reduce_max {
+extern TfLiteStatus Eval(TfLiteContext *, TfLiteNode *) { return kTfLiteOk; }
+}  // namespace reduce_max
+TfLiteRegistration *Register_REDUCE_MAX(void) {
+  static TfLiteRegistration res = {
+      nullptr,
+      nullptr,
+      nullptr,
+      reduce_max::Eval,
+  };
+  return &res;
+}
+namespace exp {
+extern TfLiteStatus Eval(TfLiteContext *, TfLiteNode *) { return kTfLiteOk; }
+}  // namespace exp
+TfLiteRegistration *Register_EXP(void) {
+  static TfLiteRegistration res = {
+      nullptr,
+      nullptr,
+      nullptr,
+      exp::Eval,
+  };
+  return &res;
+}
+namespace sum {
+extern TfLiteStatus Eval(TfLiteContext *, TfLiteNode *) { return kTfLiteOk; }
+}  // namespace sum
+TfLiteRegistration *Register_SUM(void) {
+  static TfLiteRegistration res = {
+      nullptr,
+      nullptr,
+      nullptr,
+      sum::Eval,
+  };
+  return &res;
+}
+namespace div {
+extern TfLiteStatus Eval(TfLiteContext *, TfLiteNode *) { return kTfLiteOk; }
+}  // namespace div
+TfLiteRegistration *Register_DIV(void) {
+  static TfLiteRegistration res = {
+      nullptr,
+      nullptr,
+      nullptr,
+      div::Eval,
+  };
+  return &res;
+}
+namespace squeeze {
+extern TfLiteStatus Eval(TfLiteContext *, TfLiteNode *) { return kTfLiteOk; }
+}  // namespace squeeze
+TfLiteRegistration *Register_SQUEEZE(void) {
+  static TfLiteRegistration res = {
+      nullptr,
+      nullptr,
+      nullptr,
+      squeeze::Eval,
+  };
+  return &res;
+}
+}  // namespace micro
+}  // namespace ops
+}  // namespace tflite
 
 void register_addons(tflite::ops::micro::AllOpsResolver *res) {
-    res->AddBuiltin(tflite::BuiltinOperator_REDUCE_MAX, tflite::ops::micro::Register_REDUCE_MAX());
-    res->AddBuiltin(tflite::BuiltinOperator_EXP, tflite::ops::micro::Register_EXP());
-    res->AddBuiltin(tflite::BuiltinOperator_SUM, tflite::ops::micro::Register_SUM());
-    res->AddBuiltin(tflite::BuiltinOperator_DIV, tflite::ops::micro::Register_DIV());
-    res->AddBuiltin(tflite::BuiltinOperator_SQUEEZE, tflite::ops::micro::Register_SQUEEZE());
+  res->AddBuiltin(tflite::BuiltinOperator_REDUCE_MAX,
+                  tflite::ops::micro::Register_REDUCE_MAX());
+  res->AddBuiltin(tflite::BuiltinOperator_EXP,
+                  tflite::ops::micro::Register_EXP());
+  res->AddBuiltin(tflite::BuiltinOperator_SUM,
+                  tflite::ops::micro::Register_SUM());
+  res->AddBuiltin(tflite::BuiltinOperator_DIV,
+                  tflite::ops::micro::Register_DIV());
+  res->AddBuiltin(tflite::BuiltinOperator_SQUEEZE,
+                  tflite::ops::micro::Register_SQUEEZE());
 }
