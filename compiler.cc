4c57382e5ae9509c87615f8e7a1a279745addf5d
Rafael Stahl <r.stahl@tum.de>
2020-05-20 15:29:46 +0200

    simplifications to main; use fstream over cstdio

--- compiler.cc
+++ compiler.cc
@@ -6,22 +6,7 @@
 #include "tensorflow/lite/schema/schema_generated.h"
 #include "tensorflow/lite/version.h"
 #include <iostream> // for check output
-
-// Create an area of memory to use for input, output, and intermediate arrays.
-// The size of this will depend on the model you're using, and may need to be
-// determined by experimentation.
-static int tensor_arena_size = 6 * 1024;
-static uint8_t* tensor_arena=nullptr;
-static uint8_t* g_model=nullptr;
-static int g_model_len=0;
-static char const *prefix="model_";
-
-// Set up logging.
-static tflite::ErrorReporter *error_reporter = nullptr;
-// This pulls in all the operation implementations we need.
-static tflite::ops::micro::AllOpsResolver *resolver = nullptr;
-static const tflite::Model* model = nullptr;
-static tflite::MicroInterpreter *interpreter = nullptr;
+#include <fstream>
 
 extern void dump_data(char const* prefix, tflite::MicroInterpreter *interpreter,
 	uint8_t const*tflite_array, uint8_t const* tflite_end,
@@ -29,59 +14,54 @@ extern void dump_data(char const* prefix, tflite::MicroInterpreter *interpreter,
 
 void register_addons(tflite::ops::micro::AllOpsResolver *);
 
-void init(void)
-{
-	static tflite::MicroErrorReporter micro_error_reporter;
-	error_reporter = &micro_error_reporter;
-
-	// Map the model into a usable data structure. This doesn't involve any
-	// copying or parsing, it's a very lightweight operation.
-	model = ::tflite::GetModel(g_model);
-	if (model->version() != TFLITE_SCHEMA_VERSION) {
-		TF_LITE_REPORT_ERROR(error_reporter,
-			"Model provided is schema version %d not equal "
-			"to supported version %d.\n",
-			model->version(), TFLITE_SCHEMA_VERSION);
-		return;
-	}
-	static tflite::ops::micro::AllOpsResolver local_resolver;
-	resolver= &local_resolver;
-	register_addons(resolver);
-
-	// Build an interpreter to run the model with.
-	static tflite::MicroInterpreter static_interpreter(model, *resolver, tensor_arena, tensor_arena_size, error_reporter);
-	interpreter= &static_interpreter;
-	TfLiteStatus allocate_status = interpreter->AllocateTensors();
-	if (allocate_status != kTfLiteOk) {
-		TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
-		return;
-	}
-
-	dump_data(prefix, interpreter, g_model, g_model + g_model_len, tensor_arena, tensor_arena + tensor_arena_size);
-}
-
 int main(int argc, char** argv) {
 	if (argc<2 || argc>4) {
 		std::cerr << "USAGE: " << argv[0] << " input.tflite arena_size prefix >output.cpp" << std::endl;
 		return 1;
 	}
+	int tensor_arena_size = 6 * 1024;
 	if (argc>=3) tensor_arena_size= atoi(argv[2]);
+	char const *prefix="model_";
 	if (argc>=4) prefix=argv[3];
-	FILE *f = fopen(argv[1], "rb");
+	std::ifstream f(argv[1], std::ios::binary | std::ios::ate);
 	if (!f) {
-		perror(argv[1]);
+		std::cerr << "Could not open input file\n";
 		return 2;
 	}
-	fseek(f, 0L, SEEK_END); 
-    g_model_len = ftell(f); 
-	fseek(f, 0L, SEEK_SET);
-	g_model= (uint8_t*) malloc(g_model_len);
-	if (!g_model) { std::cerr << "allocation failed" << std::endl; fclose(f); return 3; }
-	fread(g_model, 1, g_model_len, f);
-	fclose(f);
-	tensor_arena= (uint8_t *)malloc(tensor_arena_size);
-	init();
-	free(tensor_arena);
-	free(g_model);
+	size_t model_len = f.tellg();
+	f.seekg(0, std::ios::beg);
+	std::vector<uint8_t> model_buf(model_len);
+	if (!f.read((char*)model_buf.data(), model_len)) {
+		std::cerr << "Could not read input file\n";
+		return 3;
+	}
+	std::vector<uint8_t> arena_buf(tensor_arena_size);
+
+	tflite::MicroErrorReporter micro_error_reporter;
+	tflite::ErrorReporter *error_reporter = &micro_error_reporter;
+
+	// Map the model into a usable data structure. This doesn't involve any
+	// copying or parsing, it's a very lightweight operation.
+	const tflite::Model* model = ::tflite::GetModel(model_buf.data());
+	if (model->version() != TFLITE_SCHEMA_VERSION) {
+		TF_LITE_REPORT_ERROR(error_reporter,
+			"Model provided is schema version %d not equal "
+			"to supported version %d.\n",
+			model->version(), TFLITE_SCHEMA_VERSION);
+		return 4;
+	}
+	tflite::ops::micro::AllOpsResolver resolver;
+	register_addons(&resolver);
+
+	// Build an interpreter to run the model with.
+	tflite::MicroInterpreter interpreter(model, resolver, arena_buf.data(), tensor_arena_size, error_reporter);
+	TfLiteStatus allocate_status = interpreter.AllocateTensors();
+	if (allocate_status != kTfLiteOk) {
+		TF_LITE_REPORT_ERROR(error_reporter, "AllocateTensors() failed");
+		return 5;
+	}
+
+	dump_data(prefix, &interpreter, model_buf.data(), model_buf.data() + model_len, arena_buf.data(), arena_buf.data() + tensor_arena_size);
+
 	return 0;
 }
