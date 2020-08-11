b1c9efe76b7f798ce8dde03f45fb1f2fe08e0a11
Andrew Stevens <andrew.stevens@infineon.com>
2020-08-11 11:04:32 +0200

    Fix: remove another local IFX patch for different baseline tensorflow

--- src/RecordAllocations.cc
+++ src/RecordAllocations.cc
@@ -14,15 +14,13 @@ static int g_currentNodeIndex = -1;
 static uint8_t *g_arenaPtr = nullptr;
 
 static ptrdiff_t g_arena_size = 0;
-
-static TfLiteStatus LoggingAllocatePersistentBuffer(struct TfLiteContext *ctx,
-                                                    size_t bytes, void **ptr) {
-  auto retVal = g_allocator->AllocatePersistentBuffer(bytes, ptr);
-  assert(retVal == kTfLiteOk && "Alloc failure");
+                                                    size_t bytes) {
+  void* ptr = g_allocator->AllocatePersistentBuffer(bytes);
+  assert(ptr!=nullptr && "Alloc failure");
   g_loggedAllocations.push_back(
       {-(g_arenaPtr - (uint8_t *)*ptr + g_arena_size), bytes,
        g_currentNodeIndex});
-  return retVal;
+  return ptr;
 }
 static TfLiteStatus LoggingRequestScratchBufferInArena(TfLiteContext *ctx,
                                                        size_t bytes,
