1256ba226fdd08223fe145bab4e41ba4a0e7cacb
Andrew Stevens <andrew.stevens@infineon.com>
2020-09-09 18:07:33 +0200

    Fix: update for IFX_BUILD.sh to use update IFX_Makefile

--- IFX_BUILD.sh
+++ IFX_BUILD.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-make -f IFX_Makefile TARGET_ARCH=i386 clean
-make -f IFX_Makefile clean
-make -f IFX_Makefile -j 4 install
+make -f IFX_Makefile mrproper
+make -f IFX_Makefile -j 6  install
 
