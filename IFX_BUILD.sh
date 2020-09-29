ba444a34e7a8c3e93bd9df4819dd7aca9632a2b2
Andrew Stevens <andrew.stevens@infineon.com>
2020-09-29 15:51:57 +0200

    Tidy: by default do not overwrite stable installation (install_ok tag present).

--- IFX_BUILD.sh
+++ IFX_BUILD.sh
@@ -1,5 +1,32 @@
 #!/bin/bash
+set -e
+source ../SETTINGS_AND_VERSIONS.sh
+
+
+while [[ "$1" != "" && "$1" != "--" ]]
+do
+    case "$1" in
+    "--help"|"-?") 
+	echo " [--force-install] " 1>&2
+        exit 1
+        ;;
+
+    "--force-install")
+        FORCE_INSTALL=1
+        ;;
+    "*")
+        break
+        ;;
+    esac
+    shift
+done
 
 make -f IFX_Makefile mrproper
-make -f IFX_Makefile -j 6  install
+make -f IFX_Makefile -j 6  build_compiler
 
+if [ -n "$FORCE_INSTALL" -o ! -f  "${TFLITE_MICRO_ROOT}/install_ok" ]
+then
+    make -f IFX_Makefile  install
+else
+    echo "Skipping converter install to ${TFLITE_MICRO_ROOT} - install_ok tag already present" 
+fi
